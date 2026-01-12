bc6f1386cb9e9d1ddd8d4b2af253a0ff73364eb5
Guhan22M <guhan2218@gmail.com>
2026-01-12 21:26:07 +0530

    api

--- frontend/cracks/src/pages/AuthPage.js
+++ frontend/cracks/src/pages/AuthPage.js
@@ -74,7 +74,7 @@ const AuthPage = () => {
   }, [navigate]);
 
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:5000/api/auth/google"; 
+    window.location.href =`${process.env.REACT_APP_API_URL}/api/auth/google`; 
   };
 
   return (
